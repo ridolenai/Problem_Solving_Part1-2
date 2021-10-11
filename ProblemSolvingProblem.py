3e953eba1d48beb42b76518238f8d1760ce3aa08
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-10-11 14:45:15 -0500

    Fibonacci Sequence
    Works as intended.

--- ProblemSolvingProblem.py
+++ ProblemSolvingProblem.py
@@ -52,29 +52,36 @@
 #         else:
 
 
-
-
-
-
-
-
-
-
-
 #         i +=1
 
-def prime_number():
-    is_prime = ''
-    for first_number in range (2, 101):
-        is_prime = True
-        if first_number > 1:
-            for second_number in range (2, first_number):
-                if (first_number % second_number == 0 ):# and (second_number != 1):
-                    is_prime = False
-            if is_prime == True:
-                    print(first_number)
+# def prime_number():
+#     is_prime = ''
+#     for first_number in range (2, 101):
+#         is_prime = True
+#         if first_number > 1:
+#             for second_number in range (2, first_number):
+#                 if (first_number % second_number == 0 ):# and (second_number != 1):
+#                     is_prime = False
+#             if is_prime == True:
+#                     print(first_number)
                     
     
+def fibonacci():
+    number = int(input ('Enter a positive number: '))
+    first = 0
+    second = 1
+    change_variable = int ()
+    counter = 0
+    while counter < number:
+        print (first)
+        tracking_number = first + second
+        first = second
+        second = tracking_number
+        counter += 1
+        
 
 
 
+fibonacci()
+
+    
