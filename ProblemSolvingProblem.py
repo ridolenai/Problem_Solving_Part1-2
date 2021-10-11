a72c898765952061f3fb5075e0df4bdb4dea8d81
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-10-11 14:26:45 -0500

    Initial Commit

---
+++ ProblemSolvingProblem.py
@@ -0,0 +1,80 @@
+
+
+
+# split_words = ' '
+# word_phrase = []
+# caps_list = []
+# new_string = ''
+# letter_counter = ''
+# i = ''
+
+
+# def user_input ():
+#     word_phrase = input ('Please type the word or phrase you would like to use: ')
+#     selected_value = word_phrase
+#     return word_phrase
+
+# def word_reverse_tool ():
+#     new_word_phrase = ''
+#     for index in range(len(word_phrase) -1,-1,-1):
+#         new_word_phrase += word_phrase[index]
+#     print (new_word_phrase)
+
+# word_phrase = user_input()
+# word_reverse_tool()
+
+
+# def capitalize():
+#     new_string = ''
+#     caps_list = []
+#     word_phrase_string = ''
+#     word_phrase = user_input()
+#     if len (word_phrase == 0):
+#         print ('No input detected')
+#     word_phrase_list = list(word_phrase.split(' '))
+#     for word in word_phrase_list:
+#         new_word = word.capitalize()
+#         caps_list.append(new_word)
+#     for item in caps_list:
+#         new_string += item + ' '
+#     print(new_string)
+#     return (new_string)
+
+# def compression():
+#     letter_counter = (0)
+#     i = (0)
+#     index = ''
+#     user_input()
+#     while i < 1:
+
+#         if index == index + 1:
+#             letter_counter +=1
+#         else:
+
+
+
+
+
+
+
+
+
+
+
+#         i +=1
+
+def prime_number():
+    is_prime = ''
+    for first_number in range (2, 101):
+        is_prime = True
+        if first_number > 1:
+            for second_number in range (2, first_number):
+                if (first_number % second_number == 0 ):# and (second_number != 1):
+                    is_prime = False
+            if is_prime == True:
+                    print(first_number)
+                    
+    
+
+
+
